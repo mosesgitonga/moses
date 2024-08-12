1c15110568c02ae51af2c1228e1d2fa4dd273a88
mosesgitonga <infosec947@gmail.com>
2024-08-12 12:31:50 +0300

    skill update

--- moses-app/src/components/intro/Skills.jsx
+++ moses-app/src/components/intro/Skills.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Skills.css';
 
-const skills = [
-  'C', 'Python', 'JavaScript', 'SQL','MongoDB', 'HTML', 'CSS', 'React', 'nginx', 'Docker Containerization', 'SEO', 'Linux-Bash Scripting', 'Redis'
-];
+const skills = {
+  programmingLanguages: ['C', 'Python', 'JavaScript', 'SQL'],
+  frontendDevelopment: ['HTML', 'CSS', 'React'],
+  backendDevelopment: ['Python', 'SQL', 'MongoDB', 'nginx'],
+  devOpsTools: ['Docker Containerization', 'nginx', 'Linux-Bash Scripting'],
+  miscellaneous: ['SEO', 'Redis', 'Web Technologies', 'Web Scrapping', 'Software Testing']
+};
 
 const Skills = () => {
   return (
@@ -11,11 +15,16 @@ const Skills = () => {
       <div className="skills-content">
         <h1>My Skills</h1>
         <p>As a full-stack developer, I am proficient in a wide range of technologies and tools that enable me to build comprehensive and efficient applications. Here is a summary of my technical skills:</p>
-        <ul className="skills-list">
-          {skills.map((skill, index) => (
-            <li key={index} className="skill-item">{skill}</li>
-          ))}
-        </ul>
+        {Object.entries(skills).map(([category, skillList], index) => (
+          <div key={index}>
+            <h2>{category.replace(/([A-Z])/g, ' $1')}</h2>
+            <ul className="skills-list">
+              {skillList.map((skill, i) => (
+                <li key={i} className="skill-item">{skill}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
